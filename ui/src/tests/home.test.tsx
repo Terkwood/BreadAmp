5afb56755b937bdf6ceb6ac4f3a9023385ea6019
terkwood <38859656+Terkwood@users.noreply.github.com>
2020-06-06 13:37:37 -0400

    Trim

--- ui/src/tests/home.test.tsx
+++ ui/src/tests/home.test.tsx
@@ -3,12 +3,6 @@ import { h } from "preact";
 import { shallow } from "enzyme";
 import Home from "../routes/home/index";
 
-/*
-expect(wrapper.contains([
-  <span>Hello</span>,
-  <div>Goodbye</div>,
-])).to.equal(true);
-*/
 describe("Initial Test of Home", () => {
   test("Home contains three buttons", () => {
     const context = shallow(<Home />);
