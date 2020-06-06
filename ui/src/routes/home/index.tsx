1fbd728fb37eaddda126d56ec1cea050ce636b87
terkwood <38859656+Terkwood@users.noreply.github.com>
2020-06-06 13:29:19 -0400

    Stub out POST call

--- ui/src/routes/home/index.tsx
+++ ui/src/routes/home/index.tsx
@@ -1,4 +1,15 @@
 import { FunctionalComponent, h } from "preact";
+import { useState } from "preact/hooks";
+import "98.css";
+
+
+const HOST = "http://192.168.1.100:3030";
+
+const post = async (command: string) =>
+  fetch(`${HOST}/${command}`, {
+    method: "POST",
+    mode: "no-cors",
+  });
 
 const Home: FunctionalComponent = () => {
   return (
